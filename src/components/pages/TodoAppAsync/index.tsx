5404353d443d53f63921f4c8cc700ce2461139eb
arx-8 <swans.randomness@gmail.com>
2019-08-04 23:17:13 +0900

    feat: Connect state

--- src/components/pages/TodoAppAsync/index.tsx
+++ src/components/pages/TodoAppAsync/index.tsx
@@ -1,16 +1,30 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core"
+import { TodoInput } from "components/molecules/TodoInput"
+import { TodoList } from "components/molecules/TodoList"
+import { VisibilityFilterInput } from "components/molecules/VisibilityFilterInput"
+import { Todo, TodoId, VisibilityFilter } from "domain/models/Todo"
 import React from "react"
-import {
-  connect,
-  MapDispatchToPropsFunction,
-  MapStateToProps,
-} from "react-redux"
+import { connect, MapStateToProps } from "react-redux"
 import { RootState } from "store/store"
+import {
+  todoAppAsyncOperations,
+  todoAppAsyncSelectors,
+} from "store/todoAppAsync"
+import { MapThunkDispatchToPropsFunction } from "types/ReduxTypes"
 
-type ReduxStateProps = {}
+type ReduxStateProps = {
+  todoList: readonly Todo[]
+  visibilityFilter: VisibilityFilter
+}
 
-type ReduxDispatchProps = {}
+type ReduxDispatchProps = {
+  addTodo: (label: string) => void
+  changeTodoLabel: (targetId: TodoId, label: string) => void
+  changeVisibilityFilter: (visibilityFilter: VisibilityFilter) => void
+  deleteTodo: (targetId: TodoId) => void
+  toggleTodoStatus: (targetId: TodoId) => void
+}
 
 type OwnProps = {
   children?: never
@@ -18,10 +32,35 @@ type OwnProps = {
 
 type Props = OwnProps & ReduxStateProps & ReduxDispatchProps
 
-const _TodoAppAsync: React.FC<Props> = () => {
+const _TodoAppAsync: React.FC<Props> = ({
+  todoList,
+  visibilityFilter,
+  addTodo,
+  changeTodoLabel,
+  changeVisibilityFilter,
+  deleteTodo,
+  toggleTodoStatus,
+}) => {
   return (
     <div css={root}>
       <h1>TodoApp (re-ducks + async)</h1>
+      <div>
+        <TodoInput onSubmit={addTodo} />
+      </div>
+      <div css={separator}>
+        <VisibilityFilterInput
+          filterValue={visibilityFilter}
+          onChange={changeVisibilityFilter}
+        />
+      </div>
+      <div css={separator}>
+        <TodoList
+          todoList={todoList}
+          onChangeLabel={changeTodoLabel}
+          onClickDelete={deleteTodo}
+          onClickStatusToggle={toggleTodoStatus}
+        />
+      </div>
     </div>
   )
 }
@@ -30,19 +69,36 @@ const root = css`
   padding: 16px;
 `
 
-const mapStateToProps: MapStateToProps<
-  ReduxStateProps,
-  OwnProps,
-  RootState
-> = () => {
-  return {}
+const separator = css`
+  margin-top: 16px;
+`
+
+const mapStateToProps: MapStateToProps<ReduxStateProps, OwnProps, RootState> = (
+  state
+) => {
+  return {
+    todoList: todoAppAsyncSelectors.filterTodoList(state.todoAppAsync),
+    visibilityFilter: state.todoAppAsync.visibilityFilter,
+  }
 }
 
-const mapDispatchToProps: MapDispatchToPropsFunction<
+const mapDispatchToProps: MapThunkDispatchToPropsFunction<
   ReduxDispatchProps,
   OwnProps
-> = () => {
-  return {}
+> = (dispatch) => {
+  return {
+    addTodo: (label) => dispatch(todoAppAsyncOperations.addTodo({ label })),
+    changeTodoLabel: (targetId, label) =>
+      dispatch(todoAppAsyncOperations.changeTodoLabel({ targetId, label })),
+    changeVisibilityFilter: (visibilityFilter) =>
+      dispatch(
+        todoAppAsyncOperations.changeVisibilityFilter({ visibilityFilter })
+      ),
+    deleteTodo: (targetId) =>
+      dispatch(todoAppAsyncOperations.deleteTodo({ targetId })),
+    toggleTodoStatus: (targetId) =>
+      dispatch(todoAppAsyncOperations.toggleTodoStatus({ targetId })),
+  }
 }
 
 export const TodoAppAsync = connect(
